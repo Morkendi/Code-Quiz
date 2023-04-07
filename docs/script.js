b95a959ff4f1c552631a63f51e1ad38961d991ed
Daniel Sanchez <morkendi@gmail.com>
2023-04-07 13:03:44 -0600

    Fixed CSS pointer issue

--- docs/script.js
+++
@@ -1,8 +0,0 @@
-var Question1 = document.querySelector("#qstn-1")
-var StartButton = document.querySelector("#start-button")
-
-function StartQuiz() {
-    Question1.style.display = "none";
-}
-
-StartButton.addEventListener ("click", StartQuiz)
